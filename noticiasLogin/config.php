0b22a8b1b7b4e872778cbad144eaf615c23e169d
CirneS <69870121+CirneS@users.noreply.github.com>
2021-01-07 21:05:06 -0300

    Add files via upload

---
+++ noticiasLogin/config.php
@@ -0,0 +1,10 @@
+<?php
+	define("HOME_URI", "https://criacao-de-sites-joao.000webhostapp.com/noticiasLogin/");
+	define("HOME_DIR", "");
+	/*CONSTANTES DE CONEXAO AO BANCO DE DADOS */
+	define('SGBD','mysql');
+	define('HOST_DB','localhost');
+	define('USER_DB','root');
+	define('PASS_DB','vertrigo');
+	define('DB','site_noticias');
+?>
